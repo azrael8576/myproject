cf89231efa90c918ae034f1fa9d61f55baca3bd7
Alex <azrael8576love2008@gmail.com>
2018-12-18 20:58:58 +0800

    Java and Kotlin variable, data type

--- src/main/kotlin/com/alex/Hello.kt
+++ src/main/kotlin/com/alex/Hello.kt
@@ -2,8 +2,13 @@ package com.alex
 
 fun main(args: Array<String>) {
 //    println("Hello Kotlin")
-    val h= Human()
+    val h = Human()
     h.hello()
+    var age : Int = 25
+    age = age.plus(1)
+    val weight = 65.5
+    val enroll = true
+    println(enroll)
 }
 class Human{
     fun hello(){
